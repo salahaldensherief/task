9ba6b3ed3aa4a9ced5389dc3f1264b28bec4b4dc
salahaldensherief <148065524+salahaldensherief@users.noreply.github.com>
2025-06-11 13:19:25 +0300

    create sign in view body

---
+++ lib/features/auth/presentation/views/sign_in_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:task/features/auth/presentation/views/widgets/sign_in_view_body.dart';
+
+class SignInView extends StatelessWidget {
+  const SignInView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SignInViewBody();
+  }
+}
