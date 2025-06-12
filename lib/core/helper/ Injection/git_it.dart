922d54ec1892db88f3edd2c17d5bced7d2bbd8da
salahaldensherief <148065524+salahaldensherief@users.noreply.github.com>
2025-06-12 15:47:18 +0300

    create Injection file

---
+++ lib/core/helper/ Injection/git_it.dart
@@ -0,0 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:task/features/auth/data/source/user_data_source.dart';
+import 'package:task/features/auth/domain/repo/login_repo.dart';
+import 'package:task/features/auth/presentation/cubits/login_cubit/login_cubit.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerLazySingleton<UserDataSource>(() => UserDataSource());
+  getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt<UserDataSource>()));
+  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt<LoginRepo>()));
+
+}
