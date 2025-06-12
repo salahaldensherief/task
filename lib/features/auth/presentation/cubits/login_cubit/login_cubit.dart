e96b26a789066ce150510adddf69fd228f0f8199
salahaldensherief <148065524+salahaldensherief@users.noreply.github.com>
2025-06-12 14:44:45 +0300

    create login cubit done

--- lib/features/auth/presentation/cubits/login_cubit/login_cubit.dart
+++ lib/features/auth/presentation/cubits/login_cubit/login_cubit.dart
@@ -1,8 +1,23 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:task/features/auth/domain/repo/login_repo.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  LoginCubit(this.loginRepo) : super(LoginInitial());
+ final LoginRepo loginRepo;
+ Future<void> login(String email ,String password) async{
+   emit(LoginLoading());
+   try {
+     final res = await loginRepo.login(email, password);
+     if(res){
+       emit(LoginSuccess());
+     }else{
+       emit(LoginFailure('Invalid email or password'));
+     }
+   } on Exception catch (e) {
+     emit(LoginFailure('Something wrong try later'));
+   }
+ }
 }
