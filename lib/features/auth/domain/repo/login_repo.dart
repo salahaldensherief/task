84c2cdad7ea0649e79f6517180b232a8b33f7074
salahaldensherief <148065524+salahaldensherief@users.noreply.github.com>
2025-06-12 14:22:58 +0300

    create login repo done

---
+++ lib/features/auth/domain/repo/login_repo.dart
@@ -0,0 +1,11 @@
+import 'package:task/features/auth/data/source/user_data_source.dart';
+
+class LoginRepo{
+  final UserDataSource dataSource;
+
+  LoginRepo(this.dataSource);
+  Future<bool> login(String email , String password)async{
+    final user = await dataSource.getUsers();
+    return user.any((user) => user.email == email && user.password == password );
+  }
+}
