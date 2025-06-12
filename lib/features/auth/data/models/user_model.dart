408a6189992d023ed88cb828cab3cb1d8a54d6b2
salahaldensherief <148065524+salahaldensherief@users.noreply.github.com>
2025-06-12 14:04:00 +0300

    create user json file and user model

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  final String email;
+  final String password;
+
+  UserModel({required this.email,required this.password});
+
+  factory UserModel.fromJson(Map<String,dynamic> json) {
+    return UserModel(
+      email: json['email'],
+      password: json['password'],
+    );
+  }
+}
