fbcad1eb48eb360d8646babbb02a4114b1748e2c
salahaldensherief <148065524+salahaldensherief@users.noreply.github.com>
2025-06-15 14:56:24 +0300

    create best seller products cubit

---
+++ lib/features/home/presentation/cubits/products/products_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:task/features/home/data/domain/repos/products_repo.dart';
+
+import '../../../data/models/products_model.dart';
+
+part 'products_state.dart';
+
+class ProductsCubit extends Cubit<ProductsState> {
+  ProductsCubit(this.productsRepo) : super(ProductsInitial());
+  final ProductsRepo productsRepo;
+
+  void fetchProduct() async {
+    emit(ProductsLoading());
+    try {
+      List<ProductsModel> product = await productsRepo.fetchProduct();
+      emit(ProductsSuccess());
+    } on Exception catch (e) {
+      emit(ProductsFailure('something wrong try later'));
+    }
+  }
+}
---
+++ lib/features/home/presentation/cubits/products/products_state.dart
@@ -0,0 +1,13 @@
+part of 'products_cubit.dart';
+
+@immutable
+sealed class ProductsState {}
+
+final class ProductsInitial extends ProductsState {}
+final class ProductsLoading extends ProductsState {}
+final class ProductsSuccess extends ProductsState {}
+final class ProductsFailure extends ProductsState {
+  final String message;
+
+  ProductsFailure(this.message);
+}
