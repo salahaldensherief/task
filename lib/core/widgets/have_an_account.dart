f6f45c06f22ffcc7858b631d23252433b0b964e8
salahaldensherief <148065524+salahaldensherief@users.noreply.github.com>
2025-07-20 15:46:56 +0300

    svg pic

---
+++ lib/core/widgets/have_an_account.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:task/core/utils/assets.dart';
+import 'package:task/features/auth/presentation/views/sign_in_view.dart';
+
+import '../utils/app_colors.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class HaveAnAccountWidget extends StatelessWidget {
+  const HaveAnAccountWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final local = AppLocalizations.of(context)!;
+    return Text.rich(
+      TextSpan(
+        children: [
+          TextSpan(
+            text: local.alreadyHaveAccount,
+            style: TextStyle(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w900,
+              fontFamily: AppFonts.cairo,
+              color: Color(0xff808080),
+            ),
+          ),
+          TextSpan(text: '  ', ),
+
+          TextSpan(
+            recognizer:
+                TapGestureRecognizer()
+                  ..onTap = () {
+              Navigator.pushReplacementNamed(context, SignInView.routeName);
+                  },
+            text: local.login,
+            style: TextStyle(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w900,
+              fontFamily: AppFonts.cairo,
+
+              color: AppColors.primaryColor,
+            ),
+          ),
+        ],
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
